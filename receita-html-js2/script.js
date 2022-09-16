271548fe43160ac1d126ca8b404a5592c91dd25e
Lucas Silva Fernandes Canto <lucascantodev@gmail.com>
2022-09-16 15:48:21 -0300

    receita-html-js2

---
+++ receita-html-js2/script.js
@@ -0,0 +1,25 @@
+const tbody = document.querySelector('tbody');
+const botaoCarregarCervejas = document.querySelector('.botao-carregar-cervejas');
+const botaoOrdenarCervejas = document.querySelector('.botao-ordenar-cervejas');
+const botaoEmbaralharCervejas = document.querySelector('.botao-embaralhar-cervejas');
+
+const cervejas = ['Stella', 'Guiness', 'Desesperados', 'Becks'];
+
+botaoCarregarCervejas.addEventListener('click', carregarCervejas);
+botaoOrdenarCervejas.addEventListener('click', ordenarCervejas);
+botaoEmbaralharCervejas.addEventListener('click', embaralharCervejas);
+
+function carregarCervejas() {
+    tbody.innerHTML = '';
+    cervejas.map((cerveja) => (tbody.innerHTML += `<tr><td>${cerveja}</td></tr>`));
+}
+
+function ordenarCervejas() {
+    tbody.innerHTML = '';
+    cervejas.sort().map((cerveja) => (tbody.innerHTML += `<tr><td>${cerveja}</td></tr>`));
+}
+
+function embaralharCervejas() {
+    tbody.innerHTML = '';
+    cervejas.sort(() => Math.random() - 0.5).map((cerveja) => (tbody.innerHTML += `<tr><td>${cerveja}</td></tr>`));
+}
