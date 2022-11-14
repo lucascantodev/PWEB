fb522eb238773698785a1d5c09645653f3426bca
lucascantodev <lucascantodev@gmail.com>
2022-11-14 13:18:59 -0300

    Adding 'Receitas Next' folder and Receita-next-01

---
+++ Unidade02/Receitas Next/Receita-next-01/.next/server/pages/index.js
@@ -0,0 +1,66 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/Q1.js":
+/*!*********************!*\
+  !*** ./pages/Q1.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst RandomNumberCheck = ()=>{\n    const number = Math.random();\n    return console.log(\"Here's your random number sir: \" + number);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (RandomNumberCheck);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9RMS5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsTUFBTUEsb0JBQW9CLElBQU07SUFDNUIsTUFBTUMsU0FBU0MsS0FBS0MsTUFBTTtJQUN0QixPQUFPQyxRQUFRQyxHQUFHLENBQUMsb0NBQW9DSjtBQUMvRDtBQUVBLGlFQUFlRCxpQkFBaUJBLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWNlaXRhLW5leHQtMDEvLi9wYWdlcy9RMS5qcz9lOTFhIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IFJhbmRvbU51bWJlckNoZWNrID0gKCkgPT4ge1xuICAgIGNvbnN0IG51bWJlciA9IE1hdGgucmFuZG9tKCk7XG4gICAgICAgIHJldHVybiBjb25zb2xlLmxvZyhcIkhlcmUncyB5b3VyIHJhbmRvbSBudW1iZXIgc2lyOiBcIiArIG51bWJlcilcbn1cblxuZXhwb3J0IGRlZmF1bHQgUmFuZG9tTnVtYmVyQ2hlY2s7Il0sIm5hbWVzIjpbIlJhbmRvbU51bWJlckNoZWNrIiwibnVtYmVyIiwiTWF0aCIsInJhbmRvbSIsImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/Q1.js\n");
+
+/***/ }),
+
+/***/ "./pages/Q1_2.js":
+/*!***********************!*\
+  !*** ./pages/Q1_2.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"JustANumber\": () => (/* binding */ JustANumber),\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst JustANumber = ()=>{\n    return console.log(\"here's your number sir: 10\");\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (JustANumber);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9RMV8yLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sTUFBTUEsY0FBYyxJQUFNO0lBQzdCLE9BQU9DLFFBQVFDLEdBQUcsQ0FBQztBQUN2QixFQUFDO0FBRUQsaUVBQWVGLFdBQVdBLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWNlaXRhLW5leHQtMDEvLi9wYWdlcy9RMV8yLmpzPzE2ZWQiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IEp1c3RBTnVtYmVyID0gKCkgPT4ge1xuICAgIHJldHVybiBjb25zb2xlLmxvZyhcImhlcmUncyB5b3VyIG51bWJlciBzaXI6IDEwXCIpXG59XG5cbmV4cG9ydCBkZWZhdWx0IEp1c3RBTnVtYmVyOyJdLCJuYW1lcyI6WyJKdXN0QU51bWJlciIsImNvbnNvbGUiLCJsb2ciXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/Q1_2.js\n");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _Q1__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Q1 */ \"./pages/Q1.js\");\n/* harmony import */ var _Q1_2__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Q1_2 */ \"./pages/Q1_2.js\");\n\n\n\nconst Index = ()=>{\n    (0,_Q1__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n    (0,_Q1_2__WEBPACK_IMPORTED_MODULE_2__.JustANumber)();\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"hi you :D please press ctrl+shift+i\"\n    }, void 0, false, {\n        fileName: \"/home/lucas/PWEB/Unidade02/Receitas-next/Receita-next-01/pages/index.js\",\n        lineNumber: 7,\n        columnNumber: 12\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Index);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQW9DO0FBQ0Y7QUFFbEMsTUFBTUUsUUFBUSxJQUFNO0lBQ2hCRiwrQ0FBaUJBO0lBQ2pCQyxrREFBV0E7SUFDWCxxQkFBTyw4REFBQ0U7a0JBQUc7Ozs7OztBQUNmO0FBRUEsaUVBQWVELEtBQUtBLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZWNlaXRhLW5leHQtMDEvLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSYW5kb21OdW1iZXJDaGVjayBmcm9tICcuL1ExJ1xuaW1wb3J0IHtKdXN0QU51bWJlcn0gZnJvbSAnLi9RMV8yJ1xuXG5jb25zdCBJbmRleCA9ICgpID0+IHtcbiAgICBSYW5kb21OdW1iZXJDaGVjaygpO1xuICAgIEp1c3RBTnVtYmVyKCk7XG4gICAgcmV0dXJuIDxoMT5oaSB5b3UgOkQgcGxlYXNlIHByZXNzIGN0cmwrc2hpZnQraTwvaDE+O1xufVxuXG5leHBvcnQgZGVmYXVsdCBJbmRleDsiXSwibmFtZXMiOlsiUmFuZG9tTnVtYmVyQ2hlY2siLCJKdXN0QU51bWJlciIsIkluZGV4IiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ Unidade02/Receitas Next/Receita-next-01/pages/Q1_2.js
@@ -0,0 +1,5 @@
+export const JustANumber = () => {
+    return console.log("here's your number sir: 10")
+}
+
+export default JustANumber;
