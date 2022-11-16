db289c2970101c518089b6e6c17ed3a7291f03d7
lucascantodev <lucascantodev@gmail.com>
2022-11-16 10:48:04 -0300

    Revert "Adding 'Receitas Next' folder and Receita-next-01"
    This reverts commit fb522eb238773698785a1d5c09645653f3426bca.

--- Unidade02/Receitas Next/Receita-next-01/pages/Q1.js
+++
@@ -1,6 +0,0 @@
-const RandomNumberCheck = () => {
-    const number = Math.random();
-        return console.log("Here's your random number sir: " + number)
-}
-
-export default RandomNumberCheck;
--- Unidade02/Receitas Next/Receita-next-01/pages/Q1_2.js
+++
@@ -1,5 +0,0 @@
-export const JustANumber = () => {
-    return console.log("here's your number sir: 10")
-}
-
-export default JustANumber;
